d1e8380a58f1ee334d1454d1d27c988ea4db4e3f
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-08-11 10:33:38 +0530

    Implement check method to validate rotated sorted array

---
+++ Problem 1752.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int c=0,n=nums.length;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>nums[(i+1)%n]){
+                    c++;
+            }
+        }
+        return c<=1;
+    }
+}
