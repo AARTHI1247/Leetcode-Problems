90a59c1d06f104a4d133829975c5fba363d15f23
AARTHI.M <88aarthiaarthi70@gmail.com>
2026-09-04 21:11:50 +0530

    Implement method to find missing multiple in array

---
+++ Problem 3718.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++) set.add(nums[i]);
+        int val=k;
+        while(set.contains(val)){
+            val=val+k;
+        }
+        return val;
+    }
+}
